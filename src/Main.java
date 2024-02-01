2e152bbe51ef6cbdeb0ccf0810df99ac1ddf6708
AbdiJavad <Mirjavadabdi@gmail.com>
2024-02-01 20:48:39 +0330

    Updated,Changed

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+    
     public static void main(String[] args) {
         System.out.println("byte max: " + Byte.MAX_VALUE);
         System.out.println("byte min: " + Byte.MIN_VALUE);
